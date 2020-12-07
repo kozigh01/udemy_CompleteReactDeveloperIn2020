73d6bda5c98f97546525aaf3242733272376965d
mkozi <mkozi.junk@gmail.com>
2020-12-06 19:57:47 -0600

    Main app: added redux persist

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ Course: [udemy](https://www.udemy.com/course/complete-react-developer-zero-to-ma
   * React Router: [site](https://reactrouter.com/)
 * React:
   * Import image: [link](https://create-react-app.dev/docs/adding-images-fonts-and-files/)
-* Redux: [site](https://redux.js.org/) | (chrome extension)[https://github.com/zalmoxisus/redux-devtools-extension#usage]
+* Redux: [site](https://redux.js.org/) | (chrome extension)[https://github.com/zalmoxisus/redux-devtools-extension#usage] | [redux-persist](https://www.npmjs.com/package/redux-persist)
 * Firebase: [site](https://firebase.google.com/) | [get started](https://firebase.google.com/docs/web/setup?authuser=0) | [npm](https://www.npmjs.com/package/firebase)
 * UTF-8 Wingdings: [w3schools](https://www.w3schools.com/charsets/ref_utf_dingbats.asp)
 * Emoji Unicode: [w3schools](https://www.w3schools.com/charsets/ref_emoji.asp)
--- my-code/03-crwn-clothing/src/redux/store.js
+++ my-code/03-crwn-clothing/src/redux/store.js
@@ -1,16 +1,30 @@
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/es/storage'
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+
+
+// root reducer
+const persistConfig = {
+  key: 'root',
+  storage,
+  whiteList: ['cart']
+}
 
 const rootReducer = combineReducers({ 
   user: userReducer,
   cart: cartReducer 
 });
 
+const myPersistReducer = persistReducer(persistConfig, rootReducer);
+
+
+// store
 const middlewares = [ logger ];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)));
 
-export default store;
+export const store = createStore(myPersistReducer, composeEnhancers(applyMiddleware(...middlewares)));
+export const persistor = persistStore(store);
